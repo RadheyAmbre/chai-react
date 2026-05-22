132173414fc500083bf2108b2b9163c0942530d3
RadheyAmbre <radheyambre0@gmail.com>
2026-05-22 15:23:20 +0530

    Hooked Concepts Completed

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    console.log("before clicked", counter);
+    // counter = counter + 1;
+    setCounter(counter + 1);
+    console.log("after clicked", counter);
+  }
+
+    const removeValue = () => {
+    console.log("before clicked", counter);
+    // counter = counter - 1;
+    setCounter(counter - 1);
+    console.log("after clicked", counter);
+  }
+
+  if (counter == 0 || counter == 20) {
+    console.log("STOP");
+
+    removeAttribute('onClick', "#addBtn")
+  }
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button id="addBtn" onClick={addValue}>Add Value</button>
+    <br />
+    <button id="removeBtn" onClick={removeValue}>Remove Value</button>
+    <br />
+    <h2>CTA: {counter}</h2>
+
+    <h2>Footer: {counter}</h2>
+    </>
+  )
+}
+
+export default App
