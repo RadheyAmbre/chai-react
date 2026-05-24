a81dcf55be337a2e46c65f4274e8134564650f0c
RadheyAmbre <radheyambre0@gmail.com>
2026-05-24 13:22:44 +0530

    03 Tailwind and Props

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+// import Card from './component/Card.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-white-500 text mb-4">TailWind</h1>
+      {/* <Card /> */}
+    </>
+  )
+}
+
+export default App
